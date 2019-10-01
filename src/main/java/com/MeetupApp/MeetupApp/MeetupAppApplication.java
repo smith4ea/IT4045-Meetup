da49cec76dad7a7f8ca920afb62b8ff6ed258f5d
Ryan Gigliotti <torresrs@mail.uc.edu>
2019-10-01 14:47:43 -0400

    Initial Commit

---
+++ src/main/java/com/MeetupApp/MeetupApp/MeetupAppApplication.java
@@ -0,0 +1,13 @@
+package com.MeetupApp.MeetupApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetupAppApplication.class, args);
+	}
+
+}
