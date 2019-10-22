586959a2935726cb61d37f4f972a5167fceb66b0
Ryan Gigliotti <torresrs@mail.uc.edu>
2019-10-22 15:00:52 -0400

    - More backend controller

--- src/main/java/com/MeetupApp/MeetupAppController.java
+++ src/main/java/com/MeetupApp/MeetupAppController.java
@@ -1,5 +1,35 @@
 package com.MeetupApp;
 
-public class MeetupAppController {
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+public class MeetupAppController {
+	@RequestMapping(value="/meeting/{meetingId}", method=RequestMethod.GET)
+	public String getMeeting(@PathVariable int meetingId) {
+		return "NOT IMPLEMENTED";
+	}
+	
+	@RequestMapping(value="/meeting", method=RequestMethod.POST)
+	public String createMeeting() {
+		// TODO: Get new meeting object from body
+		return "NOT IMPLEMENTED";
+	}
+	
+	@RequestMapping(value="/manage-meeting-user/{meetingId}", method=RequestMethod.POST)
+	public String addUserMeeting(@PathVariable int meetingId) {
+		// TODO: Get user object from body
+		return "NOT IMPLEMENTED";
+	}
+	
+	@RequestMapping(value="/manage-meeting-user/{meetingId}/{userId}", method=RequestMethod.DELETE)
+	public String deleteUserMeeting(@PathVariable int meetingId, @PathVariable int userId) {
+		return "NOT IMPLEMENTED";
+	}
+	
+	@RequestMapping(value="/meeting", method=RequestMethod.POST)
+	public String findMeeting() {
+		// TODO: Get search meeting object from body
+		return "NOT IMPLEMENTED";
+	}
 }
--- src/main/java/com/MeetupApp/dto/UserDTO.java
+++ src/main/java/com/MeetupApp/dto/UserDTO.java
@@ -1,5 +1,40 @@
 package com.MeetupApp.dto;
 
 public class UserDTO {
-
+	int Id;
+	String firstName;
+	String lastName;
+	String email;
+	String phone;
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 }
