91cbb4571b0b45d44e9d73d098d7efc843577792
Ryan Gigliotti <torresrs@mail.uc.edu>
2019-11-12 14:56:51 -0500

    Properly wired up meeting service controller

--- src/main/java/com/MeetupApp/MeetupAppController.java
+++ src/main/java/com/MeetupApp/MeetupAppController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.MeetupApp.dto.MeetingDTO;
 import com.MeetupApp.service.IMeetingService;
 
+@RestController
 public class MeetupAppController {
 	
 	@Autowired
--- src/main/java/com/MeetupApp/service/MeetingServiceStub.java
+++ src/main/java/com/MeetupApp/service/MeetingServiceStub.java
@@ -1,8 +1,11 @@
 package com.MeetupApp.service;
 
+import org.springframework.stereotype.Component;
+
 import com.MeetupApp.dto.MeetingDTO;
 import com.MeetupApp.dto.MeetingSearchParamsDTO;
 
+@Component
 public class MeetingServiceStub implements IMeetingService {
 
 	@Override
@@ -14,7 +17,7 @@ public class MeetingServiceStub implements IMeetingService {
 	@Override
 	public MeetingDTO GetMeeting(int meetingId) {
 		// TODO Auto-generated method stub
-		return null;
+		return new MeetingDTO();
 	}
 
 	@Override
